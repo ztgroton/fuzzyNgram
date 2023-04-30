1a730fe07ce2c935f1705f2382e508f8f1bc7203
ztgroton <ztgroton@gmail.com>
2023-04-30 18:01:58 -0400

    Created utility function for cleaning whitespace in character vectors.

---
+++ R/utils-clean_whitespace.R
@@ -0,0 +1,30 @@
+
+#' Clean-Up Whitespace in Character Vectors
+#'
+#' @param x character
+#'
+#' @return character
+#'
+#' @examples
+#' \dontrun{
+#' clean_vec <- clean_whitespace(x = dirty_vec)
+#' }
+clean_whitespace <- function(x) {
+
+  # Validate Inputs
+  if (missing(x)) {stop("`x` is missing in call to `clean_whitespace`")}
+
+  # Validate Input Expectations
+  if (!isTRUE(validate.character(x))) {
+    stop("`x` must be a valid character in call to `clean_whitespace`")
+  }
+
+  # Main Logic
+
+  # * convert all contiguous whitespace to single-space / trim whitespace from start/end
+  res <- stringr::str_squish(string = x)
+
+  # Return Results
+  return(res)
+
+}
